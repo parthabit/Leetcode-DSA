fb813c5fc57ebfa0e5455ae0d822d205fb134937
Partha Das <pd6434938@gmail.com>
2026-08-13 21:50:29 +0530

    Update 05.java

--- 05.java
+++ 05.java
@@ -1,1 +1,41 @@
+class Solution {
+    public String longestPalindrome(String s) {
 
+        String answer = "";
+
+        for (int i = 0; i < s.length(); i++) {
+
+            for (int j = i + 1; j <= s.length(); j++) {
+
+                String part = s.substring(i, j);
+
+                if (isPalindrome(part)) {
+
+                    if (part.length() > answer.length()) {
+                        answer = part;
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public boolean isPalindrome(String s) {
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
