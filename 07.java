b4f92c046bd0b120d9a4255c3247332de30ee69b
Partha Das <pd6434938@gmail.com>
2026-09-01 19:27:24 +0530

    Update 07.java

--- 07.java
+++ 07.java
@@ -1,1 +1,26 @@
+class Solution {
+    public int reverse(int x) {
+        int reversed = 0;
+        
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            
+            // Check for overflow before multiplying by 10
+            // Integer.MAX_VALUE is 2147483647
+            if (reversed > Integer.MAX_VALUE / 10 || (reversed == Integer.MAX_VALUE / 10 && pop > 7)) {
+                return 0;
+            }
+            // Check for underflow before multiplying by 10
+            // Integer.MIN_VALUE is -2147483648
+            if (reversed < Integer.MIN_VALUE / 10 || (reversed == Integer.MIN_VALUE / 10 && pop < -8)) {
+                return 0;
+            }
+            
+            reversed = reversed * 10 + pop;
+        }
+        
+        return reversed;
+    }
+}
 
