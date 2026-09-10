c893f3cb20554c8819f0ac575ce1eb0986ac2b4d
Partha Das <pd6434938@gmail.com>
2026-09-10 12:23:04 +0530

    Update 02.java

--- 02.java
+++ 02.java
@@ -12,7 +12,6 @@ class Solution {
                 sum += l1.val;
                 l1 = l1.next;
             }
-
             if (l2 != null) {
                 sum += l2.val;
                 l2 = l2.next;
