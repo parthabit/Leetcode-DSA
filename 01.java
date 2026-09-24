0b7f9b793c10194746d6fa6dbf74de36dc65538c
Partha Das <pd6434938@gmail.com>
2026-09-24 19:49:05 +0530

    Update 01.java

--- 01.java
+++ 01.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
          HashMap<Integer, Integer> map = new HashMap<>();
+        
 
         for (int i = 0; i < nums.length; i++) {
             int needed = target - nums[i];
