fc240f6ff433f64ed5e8f63bc49a521e535bcb65
Partha Das <pd6434938@gmail.com>
2026-09-24 19:48:42 +0530

    Update 01.java

--- 01.java
+++ 01.java
@@ -5,7 +5,7 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             int needed = target - nums[i];
 
-            if (map.containsKey(needed)) {
+            if (map.containsKey(needed)) { 
                 return new int[]{map.get(needed), i};
             }
             map.put(nums[i], i);
